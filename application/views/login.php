d526c8f66365c04cab59e35b872b7c5a4b87ad4e
giorgiochandra <giorgio_jojo@ymail.com>
2016-09-07 20:09:27 +0700

    update modal
    ud jalan cuma masi blum ada validasi buat masuk nya

--- application/views/login.php
+++ application/views/login.php
@@ -55,19 +55,29 @@
           </div>
 
 
-          <button class="btn btn-lg btn-warning btn-block" class="log_in" type="submit" id="button-popup">Login</button>
+          <button class="btn btn-lg btn-warning btn-block" class="log_in" type="button" id="button-popup">Login</button>
 
 
     <!-- <span class="button-popup">         
             <a href="#" id="button-popup">Click Me</a></button>
           </span>  -->
-
+<!-- 
           <div class="window-popup">
             <div class="wp-content">
               <h3>Username or Password Wrong</h3> 
-              <a href="#" id="button-popup-close">Close</a>   
+              
             </div>
           </div>
+ -->
+
+          <div class="window-popup" id="popUpWindow">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal"><a href="#" id="button-popup-close">&times;</a></button>
+                  <h4 style="margin-top: 10px;">Attention</h4>   
+                </div>
+                <h3 style="font-family: 'Andada', serif; margin-top: 40px;">Username or password wrong</h3>
+            </div>
+
 
 
           <!-- <input type="submit" name="signin" value="Sign In" class="btn btn-primary"> -->
